701679990773024eb4a91081e365c0f127347547
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-04-07 10:05:02 +0700

    Add project files.

---
+++ Exercise1_162_D_Algoritma.cpp
@@ -0,0 +1,9 @@
+//1. Algoritma sangat penting dalam menyelesaikan masalah, karena dengan algoritma yang tepat kita dapat menemukan solusi yang cepat dan akurat untuk menyelesaikan masalah
+//2. Dalam algoritma itu terdapat 2 data struktur, struktur data liner dan struktur data non linear
+//3. Faktor-faktor yang mempengaruhi efisiensi suatu program algoritma execution time, seamakin dikit waktu yang dibutuhkan semakin efisien program tersebut. Memory usage, semakin dikit memory yang digunakan semakin efisien. Struktur data yang digunakan, struktu yang tepat dapat menemukan solusi dengan cepat
+//4. Algoritma yang paling efisien untuk mengurutkan data yang ukurannya kecil adalah insertion sort, karena insertion sort sederhana dan muda dipahami
+//5. Algoritma yang telah dipelajari yang masuk dalam quadraatic itu ada insertion sort, bubble sort. Sedangkan yang termasuk kedalam loglinear adalah quicksort
+
+
+#include <iostream>
+using namespace std;
